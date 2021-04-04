fa896f4667f14b00329277e53535fb771614337e
djwilkins <42594773+djwilkins@users.noreply.github.com>
2021-04-04 16:42:58 -0400

    Add another Component and Reuse three times

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+const Pet = () => {
+    return React.createElement("div", {}, [
+        React.createElement("h1", {}, "Scout"),
+        React.createElement("h2", {}, "Dog"),
+        React.createElement("h2", {}, "Chihuahua"),
+    ])
+}
+
 const App = () => {
     return React.createElement(
         "div",
-        {},
-        React.createElement("h1", {}, "Adopt Me!")
+        { id: "something-important"}, // any attributes go here
+        React.createElement("h1", {}, "Adopt Me!"),
+        React.createElement(Pet),
+        React.createElement(Pet),
+        React.createElement(Pet)
     );
 }
 ReactDOM.render(
