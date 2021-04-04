4a0d7444970f30a161a94c4d4de39209f7082fe0
djwilkins <42594773+djwilkins@users.noreply.github.com>
2021-04-04 16:48:24 -0400

    Pass props into Pet component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-const Pet = () => {
+const Pet = (props) => {
     return React.createElement("div", {}, [
-        React.createElement("h1", {}, "Scout"),
-        React.createElement("h2", {}, "Dog"),
-        React.createElement("h2", {}, "Chihuahua"),
+        React.createElement("h1", {}, props.name),
+        React.createElement("h2", {}, props.animal),
+        React.createElement("h2", {}, props.breed),
     ])
 }
 
@@ -11,9 +11,9 @@ const App = () => {
         "div",
         { id: "something-important"}, // any attributes go here
         React.createElement("h1", {}, "Adopt Me!"),
-        React.createElement(Pet),
-        React.createElement(Pet),
-        React.createElement(Pet)
+        React.createElement(Pet, { name: "Luna", animal: "Dog", breed: "Havanese"}),
+        React.createElement(Pet, { name: "Scout", animal: "Dog", breed: "Chihuahua"}),
+        React.createElement(Pet, { name: "Whiskers", animal: "Cat", breed: "Mixed"})
     );
 }
 ReactDOM.render(
