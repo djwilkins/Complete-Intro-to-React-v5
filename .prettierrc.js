e7aca728e7b1073793af8ee6a1f0dd21a63444d0
djwilkins <42594773+djwilkins@users.noreply.github.com>
2021-04-10 13:32:51 -0400

    Added prettier config file
    Also, as comments in the prettier config file detail, configured vsCode to apply its formatting to files in the project on save.

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+{} // An empty object accepts the defautl prettier configs
+// Can add desired parameters to object (see prettier docs)
+
+// Along with creating this file,
+// I configured vsCode from:
+// Code > Preferences > Settings
+// to (1) Format on Save (check)
+// (2) Prettier: Require Config (check)
+// The later which only runs prettier on save
+// When a prettier config file exists in the project
