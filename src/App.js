75dc9f80159ac12596aae12c443ef4f3bc089768
djwilkins <42594773+djwilkins@users.noreply.github.com>
2021-04-10 13:22:56 -0400

    Added format command to node package scripts and ran it
    Added format command to node package.json "scripts".  It executes prettier against js and html files in the src folder.
    
    Once added to package.json "scripts", was able to run from terminal in project folder with "$ npm run format", which I did before this commit (see src/App.js and src/index.html changes).

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 const Pet = ({ name, animal, breed }) => {
-    return React.createElement("div", {}, [
-        React.createElement("h1", {}, name),
-        React.createElement("h2", {}, animal),
-        React.createElement("h2", {}, breed),
-    ])
-}
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h2", {}, breed),
+  ]);
+};
 
 const App = () => {
-    return React.createElement(
-        "div",
-        { id: "something-important"}, // any attributes go here
-        React.createElement("h1", {}, "Adopt Me!"),
-        React.createElement(Pet, { name: "Luna", animal: "Dog", breed: "Havanese"}),
-        React.createElement(Pet, { name: "Scout", animal: "Dog", breed: "Chihuahua"}),
-        React.createElement(Pet, { name: "Whiskers", animal: "Cat", breed: "Mixed"})
-    );
-}
-ReactDOM.render(
-    React.createElement(App),
-    document.getElementById("root")
-);
+  return React.createElement(
+    "div",
+    { id: "something-important" }, // any attributes go here
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havanese",
+    }),
+    React.createElement(Pet, {
+      name: "Scout",
+      animal: "Dog",
+      breed: "Chihuahua",
+    }),
+    React.createElement(Pet, {
+      name: "Whiskers",
+      animal: "Cat",
+      breed: "Mixed",
+    })
+  );
+};
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
