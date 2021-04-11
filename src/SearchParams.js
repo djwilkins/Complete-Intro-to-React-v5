5cf5b2189220a3aa66fd7a263ae7a9a1e06d22a0
djwilkins <42594773+djwilkins@users.noreply.github.com>
2021-04-11 14:04:37 -0400

    Create custom useDropdown Hook
    Here we create an abstracted useDropdown Hook (useDropdown.js) so we don't have to repeat ourselves in creating similar dropdowns (as we had for "Animal" and "Breed" in SearchParams.js).
    
    In so doing, we then utilize this in SearchParams.js and replace the redundant code there.

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import { ANIMALS } from '@frontendmasters/pet';
+import useDropdown from './useDropdown';
 
 const SearchParams = () => {
   const [location, setLocation] = useState('Seattle, WA');
-  const [animal, setAnimal] = useState('dog');
-  const [breed, setBreed] = useState('');
+  const [animal, AnimalDropDown] = useDropdown('Animal', 'dog', ANIMALS);
+  const [breed, BreedDropDown] = useDropdown('Breed', '', []);
   const [breeds, setBreeds] = useState([]);
 
   return (
@@ -19,37 +20,8 @@ const SearchParams = () => {
             onChange={(event) => setLocation(event.target.value)}
           />
         </label>
-        <label htmlFor="animal">
-          Animal
-          <select
-            id="animal"
-            value={animal}
-            onChange={(event) => setAnimal(event.target.value)}
-          >
-            <option>All</option>
-            {ANIMALS.map((animal) => (
-              <option key={animal} value={animal}>
-                {animal}
-              </option>
-            ))}
-          </select>
-        </label>
-        <label htmlFor="breed">
-          Breed
-          <select
-            id="breed"
-            value={breed}
-            onChange={(event) => setBreed(event.target.value)}
-            disabled={breeds.length === 0}
-          >
-            <option>All</option>
-            {breeds.map((breedString) => (
-              <option key={breedString} value={breedString}>
-                {breedString}
-              </option>
-            ))}
-          </select>
-        </label>
+        <AnimalDropDown/>
+        <BreedDropDown/>
         <button>Submit</button>
       </form>
     </div>
