4e44eb0601674685c4b04824d9c0b150e81e204f
djwilkins <42594773+djwilkins@users.noreply.github.com>
2021-04-11 12:41:05 -0400

    Install and import ANIMALS from frontendmasters/pet
    With parcel serving our project ($ npm run dev # still running), adding the new import line we added to src/SearchParams.js here will automatically npm install the source for it for us (as reflected in update made to package.json and package-lock.json here.)

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { ANIMALS } from "@frontendmasters/pet";
 
 const SearchParams = () => {
     const [location, setLocation] = useState("Seattle, WA");
