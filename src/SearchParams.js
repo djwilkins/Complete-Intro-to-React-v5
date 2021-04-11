ba5737ce8821fa39986cb5a12a4f51c751873ba9
djwilkins <42594773+djwilkins@users.noreply.github.com>
2021-04-11 13:00:20 -0400

    Update SearchParams.js with Animal Dropdown
    Added Animal select dropdown to SearchParam component.  Used useState and onChange utilization of its update method (same as had done for location input label).
    
    Also, populated options in Animal select dropdown from imported ANIMALS array using vanilla JS array map method.

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import { ANIMALS } from "@frontendmasters/pet";
+import { option } from 'commander';
 
 const SearchParams = () => {
     const [location, setLocation] = useState("Seattle, WA");
+    const [animal, setAnimal] = useState("dog");
     // array destructuring [current state, update state]
     // = useState(initial default value)
 
@@ -13,6 +15,14 @@ const SearchParams = () => {
                     Location
                     <input id="location" value={location} 
                     placeholder="Location" onChange={event => setLocation(event.target.value)} />
+                </label>
+                <label htmlFor="animal">
+                    Animal
+                    <select id="animal" value={animal}
+                    onChange={event => setAnimal(event.target.value)}>
+                        <option>All</option>
+                        {ANIMALS.map(animal => <option value={animal}>{animal}</option>)}
+                    </select>
                 </label>
                 <button>Submit</button>
             </form>
