011570c295e53dbdc049c9024552cdd279af08a4
djwilkins <42594773+djwilkins@users.noreply.github.com>
2021-04-17 15:08:53 -0400

    Add useEffect hook - console.log test version
    See accompanying comments for some info on useEffect

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { ANIMALS } from '@frontendmasters/pet';
+import React, { useState, useEffect } from 'react';
+import pet, { ANIMALS } from '@frontendmasters/pet';
 import useDropdown from './useDropdown';
 
 const SearchParams = () => {
@@ -7,6 +7,12 @@ const SearchParams = () => {
   const [animal, AnimalDropDown] = useDropdown('Animal', 'dog', ANIMALS);
   const [breed, BreedDropDown] = useDropdown('Breed', '', []);
   const [breeds, setBreeds] = useState([]);
+
+  useEffect(() => {
+      pet.breeds("dog").then(console.log, console.error);
+  }) // useEffect schedules its method to run after the component has renderered the 1st time
+  // That's to not delay the user's initial load for a better experience
+  // Note: useEffect replaces a number of older react component methods
 
   return (
     <div className="search-params">
