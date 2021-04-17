4d21aee2c5d421a52ab90ba7a546b02027f20737
djwilkins <42594773+djwilkins@users.noreply.github.com>
2021-04-17 15:39:40 -0400

    Add useEffect hook - fully implemented
    See comment lines for full explanation of implementation

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -5,14 +5,29 @@ import useDropdown from './useDropdown';
 const SearchParams = () => {
   const [location, setLocation] = useState('Seattle, WA');
   const [animal, AnimalDropDown] = useDropdown('Animal', 'dog', ANIMALS);
-  const [breed, BreedDropDown] = useDropdown('Breed', '', []);
   const [breeds, setBreeds] = useState([]);
+  const [breed, BreedDropDown, setBreed] = useDropdown('Breed', '', breeds);
+  // The "breeds" array passed to the BreedDropDown as options above (see useDropdown.js)
+  // is initially defined as an empty array - but is subsequently
+  // Populated by the useEffect method below setting "Breeds" equal an array of breed names
+  // Returned by the pet api breed method - first, for the default animal (above)
+  // And again subsequently for any other chosen animal/AnimalDropdown.
 
   useEffect(() => {
-      pet.breeds("dog").then(console.log, console.error);
-  }) // useEffect schedules its method to run after the component has renderered the 1st time
+      setBreeds([]);
+      setBreed("");
+
+      pet.breeds(animal).then(({ breeds }) => {
+          const breedStrings = breeds.map(({ name }) => name);
+          setBreeds(breedStrings);
+      }, console.error);
+  }, [animal, setBreed, setBreeds]);
+  // useEffect schedules its method to run after the component has renderered the 1st time
   // That's to not delay the user's initial load for a better experience
   // Note: useEffect replaces a number of older react component methods
+  // The dependencies pased to useEffect as the second parameter above
+  // Lets react now that only when any of these dependencies change
+  // Does the useEffect method need to run again.
 
   return (
     <div className="search-params">
