bb37184057bb0459b81ff539d548fe57a201536e
djwilkins <42594773+djwilkins@users.noreply.github.com>
2021-04-18 11:00:02 -0400

    Simple comment

--- src/Pet.js
+++ src/Pet.js
@@ -3,6 +3,7 @@ import React from 'react';
 export default function Pet({ name, animal, breed, media, location, id }) {
 
     let hero = 'http://placecorgi.com/300/300';
+    // If pet has image, replace place holder with real image
     if(media.length) {
         hero = media[0].small;
     }
