f01c45ea6895aa8728210a42a0b85c2315939d70
djwilkins <42594773+djwilkins@users.noreply.github.com>
2021-04-18 11:36:17 -0400

    React Router - Display Props to Screen Trick
    Just a fun trick for development / debugging.

--- src/Details.js
+++ src/Details.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 const Details = () => {
-    return <h1>hi lol</h1>
+    const props = useParams();
+    // Cool trick for displaying props values visually on page (just for development/debugging)
+    return (
+        <pre>
+            <code>{JSON.stringify(props, null, 4)}</code>
+        </pre>
+    )
 }
 
 export default Details;
